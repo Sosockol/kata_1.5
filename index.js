fff07ddaf376e08c4ad7d4d054dbbad7235f63a3
Dmitrii Bolokhontsev <bolokhontsev.main@gmail.com>
2024-03-07 18:04:34 +0300

    Теперь js показывает после загрузки только нужный под разрешение блок

--- index.js
+++ index.js
@@ -8,13 +8,29 @@ const slider = new Swiper('.mySwiper', {
   slidesPerView: 'auto',
 });
 
-window.addEventListener("resize", () => {
-  if (768 <= window.innerWidth) {
-    document.querySelector('.info__swiper').hidden = true;
-    document.querySelector('.info__expandable').hidden = false;
+/* Ничего умнее не придумал что бы вначале не загружались сразу оба блока */
+document.querySelector('.info__swiper').hidden = true;
+document.querySelector('.info__expandable').hidden = true;
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (768 > window.screen.width) {
+    document.querySelector('.info__swiper').classList.remove('hideClass');
+    document.querySelector('.info__expandable').classList.remove('hideClass');
+    document.querySelector('.info__swiper').hidden = false;
   } else {
+    document.querySelector('.info__expandable').classList.remove('hideClass');
+    document.querySelector('.info__swiper').classList.remove('hideClass');
+    document.querySelector('.info__expandable').hidden = false;
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (768 > window.innerWidth) {
     document.querySelector('.info__swiper').hidden = false;
     document.querySelector('.info__expandable').hidden = true;
+  } else {
+    document.querySelector('.info__swiper').hidden = true;
+    document.querySelector('.info__expandable').hidden = false;
   }
 });
 
